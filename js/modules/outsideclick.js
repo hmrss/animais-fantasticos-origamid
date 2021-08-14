9fc6da973204c600bc4fe0375fb498905389e0e0
hmrss <henriquemarussi@outlook.com>
2021-08-14 12:17:10 -0300

    Menu mobile

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -4,7 +4,7 @@ export default function outsideClick(element, events, callback) {
 
   if (!element.hasAttribute(outside)) {
     events.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutsideClick);
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
     });
     element.setAttribute(outside, "");
   }
@@ -14,7 +14,7 @@ export default function outsideClick(element, events, callback) {
       element.removeAttribute(outside);
 
       events.forEach((userEvent) => {
-        html.addEventListener(userEvent, handleOutsideClick);
+        html.removeEventListener(userEvent, handleOutsideClick);
       });
       callback();
     }
