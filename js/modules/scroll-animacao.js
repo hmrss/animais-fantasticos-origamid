4ec17386bb6c1393fa5974df4dada29c680fbd8c
hmrss <henriquemarussi@outlook.com>
2021-08-14 13:22:11 -0300

    Section Anima Numeros

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -7,7 +7,11 @@ export default function initAnimacaoScroll() {
       const { top } = section.getBoundingClientRect();
       const isSectionVisible = top - halfView < 0;
 
-      if (isSectionVisible) section.classList.add("ativo");
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
     });
   }
 
