51d50a63d9d479b485b6340c56b9d3b50b06dce3
hmrss <henriquemarussi@outlook.com>
2021-08-28 11:02:48 -0300

    Fetch numeros animais

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,26 @@
+import initAnimaNumeros from "./anima-numero.js";
+
+export default function initFetchAnimais() {
+  const url = "./animaisapi.json";
+  async function fetchAnimais(url) {
+    const animaisResponse = await fetch(url);
+    const dataAnimais = await animaisResponse.json();
+    const numerosGrid = document.querySelector(".numeros-grid");
+
+    dataAnimais.forEach((animal) => {
+      const divAnimal = createAnimal(animal);
+      numerosGrid.appendChild(divAnimal);
+    });
+    console.log("passou");
+    initAnimaNumeros();
+  }
+
+  function createAnimal({ specie, total }) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+    div.innerHTML = `<h3>${specie}</h3><span data-numero>${total}</span>`;
+    return div;
+  }
+
+  fetchAnimais(url);
+}
