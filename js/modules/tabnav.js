906f9c56e06aa5e4c61608450e5c3dd1a0d5224e
hmrss <henriquemarussi@outlook.com>
2021-09-28 15:50:45 -0300

    Tabnav to Class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,16 +1,35 @@
-export default function initTabNav() {
-  const tabmenu = document.querySelectorAll("[data-tab='menu'] li");
-  const tabcontent = document.querySelectorAll("[data-tab='content'] section");
-
-  function activeTab(index) {
-    tabcontent.forEach((content) => content.classList.remove('ativo'));
-    const direction = tabcontent[index].dataset.anime;
-
-    tabcontent[index].classList.add('ativo');
-    tabcontent[index].classList.add(direction);
+export default class TabNav {
+  constructor(menu, content) {
+    // Menu com as imagens
+    this.tabmenu = document.querySelectorAll(menu);
+    // Conteudo que sera exibido ao clicar na imagem
+    this.tabcontent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  tabmenu.forEach((li, i) => {
-    li.addEventListener('click', () => activeTab(i));
-  });
+  // Ativa a tab de acordo com o index passado
+  activeTab(index) {
+    // direction = valor do tipo da animacao para adicionar a class ao css
+    const direction = this.tabcontent[index].dataset.anime;
+    // remove a class de todas os conteudos
+    this.tabcontent.forEach((content) =>
+      content.classList.remove(this.activeClass),
+    );
+    // ativa o conteudo ao index passado
+    this.tabcontent[index].classList.add(this.activeClass, direction);
+  }
+
+  // Adiciona os eventos de click as imagens
+  addEvent() {
+    this.tabmenu.forEach((li, i) => {
+      li.addEventListener('click', () => this.activeTab(i));
+    });
+  }
+
+  init() {
+    if (this.tabmenu.length && this.tabcontent.length) {
+      this.activeTab(0); // ativa a primeira imagem
+      this.addEvent();
+    }
+  }
 }
