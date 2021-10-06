adf5efb6e905804072f1a178f59d9f8cbe0f67ad
hmrss <henriquemarussi@outlook.com>
2021-10-06 16:16:06 -0300

    fetchBitcoin Reafatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,12 +1,12 @@
-export default function initFetchBitcoin() {}
-
-fetch('https://blockchain.info/ticker')
-  .then((response) => response.json())
-  .then((bitcoin) => {
-    const btcValue = document.querySelector('.btc-preco');
-    /* Mil reais em bitcoin */
-    btcValue.innerText = (1000 / bitcoin.BRL.buy).toFixed(4);
-  })
-  .catch((erro) => {
-    console.log(Error(erro));
-  });
+export default function fetchBitcoin(url, target, value) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcValue = document.querySelector(target);
+      /* Value em reais */
+      btcValue.innerText = (value / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
